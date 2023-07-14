123497b2fb9b27fe23a7bce62209eb201fa98064
Hoan Truong <hoantruong6814@gmail.com>
2023-07-14 20:39:25 +0700

    handle swipe item to remove article saved to room

--- app/src/main/java/com/hoantruong6814/news/ui/fragment/SaveNewsFragment.kt
+++ app/src/main/java/com/hoantruong6814/news/ui/fragment/SaveNewsFragment.kt
@@ -5,7 +5,10 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.hoantruong6814.news.R
 import com.hoantruong6814.news.adapter.NewsAdapter
 import com.hoantruong6814.news.databinding.FragmentSaveNewsBinding
@@ -36,7 +39,43 @@ class SaveNewsFragment : Fragment(R.layout.fragment_save_news) {
                 putSerializable("article", it);
             }
 
-            findNavController().navigate(R.id.action_saveNewsFragment_to_detailNewsFragment, bundle);
+            findNavController().navigate(
+                R.id.action_saveNewsFragment_to_detailNewsFragment,
+                bundle
+            );
+        }
+
+        val itemTouchHelperCallback =
+            object :
+                ItemTouchHelper.SimpleCallback(
+                    ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return true;
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val position = viewHolder.layoutPosition;
+                    val article = newsAdapter.differ.currentList[position];
+                    viewModel.deleteArticle(article);
+                    Snackbar.make(view, "Article is deleted.", Snackbar.LENGTH_SHORT).apply {
+                        setAction("Undo") {
+                            viewModel.saveArticle(article);
+                        }
+                        show()
+                    }
+                }
+
+            }
+
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.rvNewsSaved);
         }
     }
 
